ce615419dfe9be3150a0dbb36d0ba1ad3942e39e
Gleb Kushedow <kushedow@gmail.com>
2025-02-13 19:26:18 +0300

    fix statuses in case no student_id found

--- components/sections/status_section.js
+++ components/sections/status_section.js
@@ -18,21 +18,21 @@ function StatusSection(store) {
                 </div>                    
           </div>    
           
-           <div v-if="store.sections.bio !='loading' && model.hh_access_token==''">    
+           <div v-if="model.student_id && store.sections.bio !='loading' && model.hh_access_token==''">    
                 <div class="alert alert-info clearfix">
                 Данные загружены, анкета не подключена к HeadHunter             
                     <input type="text" v-model="model.hh_access_token"  class="form-control float-end w-25" placeholder="hh_access_code">         
                 </div>                    
            </div>     
                  
-           <div v-if="model.hh_access_token">    
+           <div v-if="model.student_id && model.hh_access_token">    
                 <div class="alert alert-success clearfix">
                 Данные загружены, доступ к HeadHunter предоставлен            
                     <input type="text" v-model="model.hh_access_token"  class="form-control float-end w-25" placeholder="hh_access_code">         
                 </div>                    
            </div> 
            
-           <div v-if="store.sections.bio !='loading' && model.hh_access_token==''"> 
+           <div v-if="model.student_id && store.sections.bio !='loading' && model.hh_access_token==''"> 
             <div class="alert alert-info text-muted mt-3 clearfix">
              <small>Предоставьте нам доступ к HH чтобы продолжить </small>
              <a class="float-end btn btn-primary" :href="'https://hh.ru/oauth/authorize?response_type=code&client_id='+model.hh_client_id" class="btn btn-dark">🔐 Предоставить доступ</a> 
